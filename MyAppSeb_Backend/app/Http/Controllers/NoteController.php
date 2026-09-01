10523dc36bad69c1311d622c215f838d92eb8939
SebaJeldres <Sjeldresosoriodev@gmail.com>
2026-09-01 18:32:51 -0400

    Mejora los mensajes de respuesta en NoteController y añade manejo de errores en el método update

--- MyAppSeb_Backend/app/Http/Controllers/NoteController.php
+++ MyAppSeb_Backend/app/Http/Controllers/NoteController.php
@@ -19,7 +19,7 @@ class NoteController extends Controller
             $notes = Note::all();
 
             return response()->json([
-                'message' => 'notas traidas con exito',
+                'message' => 'Notas traidas con exito',
                 'notes' => $notes,
                 'status' => 200
             ], 200);
@@ -65,7 +65,7 @@ class NoteController extends Controller
             ]);
 
             return response()->json([
-                'message' => 'nota creada con exito',
+                'message' => 'Nota creada con exito',
                 'notes' => $note,
                 'status' => 201
             ], 201);
@@ -91,13 +91,13 @@ class NoteController extends Controller
 
             if (!$note) {
                 return response()->json([
-                    'message' => 'nota no encontrada',
+                    'message' => 'Nota no encontrada',
                     'status' => 404
                 ], 404);
             }
 
             return response()->json([
-                'message' => 'nota extraida con exito',
+                'message' => 'Nota extraida con exito',
                 'note' => $note,
                 'status' => 200
             ], 200);
@@ -116,9 +116,58 @@ class NoteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Note $note)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'title' => 'sometimes|string|max:100',
+                'description' => 'nullable|string',
+                'date' => 'sometimes|date',
+                'status' => 'sometimes|boolean',
+                'prioridad' => 'sometimes|in:baja,media,alta'
+            ]);
+
+            if ($validator->fails()) {
+
+                return response()->json([
+                    'message' => 'Datos invalidos',
+                    'errors' => $validator->errors(),
+                    'status' => 422,
+                ], 422);
+            }
+
+            $note = Note::find($id);
+
+            if (!$note) {
+                return response()->json([
+                    'message' => 'Nota no encontrada',
+                    'status' => 404
+                ], 404);
+            }
+
+            $note->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'date' => $request->date,
+                'status' => $request->status,
+                'prioridad' => $request->prioridad,
+            ]);
+
+            return response()->json([
+                'message' => 'Nota actualizada con exito',
+                'note' => $note,
+                'status' => 200
+            ], 200);
+
+
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error al actualizar la nota',
+                'error' => $e->getMessage(),
+                'status' => 500
+            ], 500);
+        }
     }
 
     /**
